060c7d729927a7b9f35c661ed47f7c76baf308f6
UkadanAli <45583190+Ukadan@users.noreply.github.com>
2022-12-20 08:38:17 +0600

    Add files via upload

---
+++ build/eCommerceT3test.build/Debug-iphonesimulator/eCommerceT3test.build/DerivedSources/CoreDataGenerated/eCommerceT3test/eCommerceT3test+CoreDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  eCommerceT3test+CoreDataModel.swift
+//  
+//
+//  Created by Ali on 17.12.2022.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+
